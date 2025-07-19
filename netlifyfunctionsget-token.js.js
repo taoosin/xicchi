97193262a0e3ae6a26788b6435553aa7add2c483
taoosin <taosingfx@gmail.com>
2025-07-19 22:26:38 +0600

    Delete netlifyfunctionsget-token.js.js

--- netlifyfunctionsget-token.js.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/get-token.js
-
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: process.env.GITHUB_TOKEN || "No token set"
-  };
-};
